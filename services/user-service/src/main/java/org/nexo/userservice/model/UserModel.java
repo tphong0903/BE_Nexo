7420d121a0a78434bd8f6a9084c40269aae7c9f0
QuangDang <tanvinh58@gmail.com>
2026-06-09 00:35:14 +0700

    feat: add UserModel JPA entity for user profile management

--- services/user-service/src/main/java/org/nexo/userservice/model/UserModel.java
+++ services/user-service/src/main/java/org/nexo/userservice/model/UserModel.java
@@ -6,7 +6,6 @@ import org.nexo.userservice.enums.ERole;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import org.hibernate.annotations.Check;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
@@ -20,7 +19,6 @@ import java.util.List;
 
 @Entity
 @Table(name = "users")
-@Check(name = "users_account_status_check", constraints = "account_status IN ('PENDING', 'ACTIVE', 'INACTIVE', 'LOCKED', 'UPDATE')")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor

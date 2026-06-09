ea45209993196998bce424cd3488ed0d3cc5c51d
QuangDang <tanvinh58@gmail.com>
2026-06-09 08:05:59 +0700

    feat: create UserModel JPA entity for user management

--- services/user-service/src/main/java/org/nexo/userservice/model/UserModel.java
+++ services/user-service/src/main/java/org/nexo/userservice/model/UserModel.java
@@ -64,11 +64,11 @@ public class UserModel {
     @Builder.Default
     private Boolean isKOL = false;
 
-    @Column(name = "account_status")
+    @Column(name = "account_status", columnDefinition = "varchar(255)")
     @Enumerated(EnumType.STRING)
     private EAccountStatus accountStatus;
 
-    @Column(name = "role")
+    @Column(name = "role", columnDefinition = "varchar(255)")
     @Enumerated(EnumType.STRING)
     @Builder.Default
     private ERole role = ERole.USER;
